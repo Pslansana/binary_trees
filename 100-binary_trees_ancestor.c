3f2b56f9bdd2986f1650aa67ab17c25c085d7715
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-03-28 03:21:43 -0700

    binary trees

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -22,4 +22,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		return (binary_trees_ancestor(first, pop));
 	else if (mom == second || !pop || (!pop->parent && mom))
 		return (binary_trees_ancestor(mom, second));
-	return (binary_trees_ancestor(mom, pop))
+	return (binary_trees_ancestor(mom, pop));
+}
